14b2b9a286fd53db99335ec9c183b830c73c7b9b
Vickybarai <baraivicky15@gmail.com>
2026-03-03 12:23:21 +0530

    tf

--- Terraform/code/day-10/modules/s3/s3.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_s3_bucket" "remote_s3" {
-#     bucket = "${var.env}-${var.bucket}-remote" 
-    
-#     tags = {
-#         Name = "${var.env}-${var.bucket}-remote"
-#         environment = var.env
-#     }
-  
-# }
