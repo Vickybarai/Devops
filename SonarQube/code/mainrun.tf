9b75678283b1b5c17bb73e318b9ebcba63317e81
Vickybarai <baraivicky15@gmail.com>
2026-03-13 12:11:42 +0530

    none

--- SonarQube/code/mainrun.tf
+++ SonarQube/code/mainrun.tf
@@ -9,17 +9,17 @@ module "sonarqube" {
   instance_name = "instance"
   environment_name = "sonar"
   ami_value = "ami-0938a60d87953e820"
-  instance_type_value = "t3.micro"
+  instance_type_value = "c7i-flex.large"
   instance_key_name = "TF-key"
   vpc_security_group_ids = ["sg-0c34f567e22c52e77"]
   subnet_id_value = ["subnet-016c5cd1838c909f4"]
-  storage_size = 9
+  storage_size = 20
 
  user_data = <<-EOF
 #!/bin/bash
 sudo apt update -y
 sudo apt install openjdk-17-jdk postgresql -y
-
+systemctl start postgresql
 
 EOF
 
